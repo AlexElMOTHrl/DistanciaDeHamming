7eb07aa282b0e9451251e5f912a3d8c2254dc095
AlexElMOTHrl <vivamoth93@gmail.com>
2023-01-10 16:44:30 +0100

    Fixed Alerts

--- Program.cs
+++ Program.cs
@@ -1,13 +1,16 @@
-﻿
+﻿//Calcular distancia de Hamming
 internal class Program
 {
     static public string word1 = "";
     static public string word2 = "";
+    static public string? word1Initial = "";
+    static public string? word2Initial = "";
     static public int error = 0;
     static public int distance = 0;
     static private float porcentajeError = 0;
     private static void Main(string[] args)
     {
+        Console.Clear();
         Console.WriteLine("Calcular distancia de Hamming.");
 
         bool isValid = false;
@@ -17,13 +20,29 @@ internal class Program
             {
                 Console.Clear();
                 Console.Write("Primera palabra/número...\n> ");
-                word1 = Console.ReadLine().ToLower();
+                word1Initial = Console.ReadLine();
+                if (word1Initial != null)
+                {
+                    word1 = word1Initial.ToLower();
+                }
+                else
+                {
+                    word1Initial = "None".ToLower();
+                }
             } while (word1.Length <= 0);
             do
             {
                 Console.Clear();
                 Console.Write("Segunda palabra/número...\n> ");
-                word2 = Console.ReadLine().ToLower();
+                if (word2Initial != null)
+                {
+                    word1 = word2Initial.ToLower();
+                }
+                else
+                {
+                    word2Initial = "None".ToLower();
+                }
+                word2 = word2Initial.ToLower();
             } while (word2.Length <= 0);
 
             isValid = ValidateLength(word1, word2);
@@ -33,14 +52,16 @@ internal class Program
         error = GetError(word1, word2);
         porcentajeError = PorcentajeError(word1, word2);
 
-        //?Console.WriteLine($"DEBUG:\nDistancia = {word1.Length}\nError = {error}\n");
+        //?Console.WriteLine($"\nDEBUG:\nDistancia = {word1.Length}\nError = {error}\n");
 
         Console.Clear();
-        Console.WriteLine($"El error entre \"{word1}\" y \"{word2}\" es de {error}.");
+        Console.WriteLine($"El error entre \"{word1Initial}\" y \"{word2Initial}\" es de {error}...\n");
         Console.BackgroundColor = GetBackgroundColor(porcentajeError);
-        Console.Write($"(Similitud del {Math.Round(porcentajeError, 2)}%)");
+        Console.Write($"(Similitud del {Math.Round(porcentajeError, 2)}%)\n");
         Console.ResetColor();
-        Console.WriteLine("...\n");
+        Console.CursorVisible = false;
+        Console.ReadKey();
+        Console.CursorVisible = true;
     }
 
     static public int HammingDistance(string s1, string s2)
@@ -83,9 +104,7 @@ internal class Program
         if (s1.Length != s2.Length)
         {
             Console.Clear();
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Las dos palabras tienen que contener el mismo número de caracteres, si necesitas que sean de distinto tamaño puedes añadir espacios (Contarán como error y puede calcularse mal.)\nPulsa (Y) para continuar con las palabras anteriores.");
-            Console.ResetColor();
+            WriteError("Las dos palabras tienen que contener el mismo número de caracteres, si necesitas que sean de distinto tamaño puedes añadir espacios.\n(Contarán para el error)\n\nSi desea continuar con las palabras anteriores pulsa (Y) ¡SE CALCULARA MAL O CRASHEARÁ!.");
             input = Console.ReadKey();
             if (input.Key == ConsoleKey.Y)
             {
@@ -100,6 +119,13 @@ internal class Program
         {
             return true;
         }
+    }
+
+    static void WriteError(string msg)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(msg);
+        Console.ResetColor();
     }
 
     static ConsoleColor GetBackgroundColor(float similarity)
